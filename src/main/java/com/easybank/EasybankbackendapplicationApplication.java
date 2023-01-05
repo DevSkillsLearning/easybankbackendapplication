18dfef7776e021c4252da0f651135e6bc1c18f46
Devang Chauhan <chauhan.devam@gmail.com>
2023-01-05 21:29:06 +0530

    EZB-4: implemented a custom security requirements

--- src/main/java/com/easybank/EasybankbackendapplicationApplication.java
+++ src/main/java/com/easybank/EasybankbackendapplicationApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasybankbackendapplicationApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EasybankbackendapplicationApplication.class, args);
     }
-
 }
