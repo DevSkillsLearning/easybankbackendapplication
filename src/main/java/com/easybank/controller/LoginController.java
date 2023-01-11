42e622352cab88309dba2b35c2cd5c6ef40f9fbe
Devang Chauhan <chauhan.devam@gmail.com>
2023-01-11 18:47:47 +0530

    EZB-10: implemented a REST api to allow the registration of new user

---
+++ src/main/java/com/easybank/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.easybank.controller;
+
+
+import com.easybank.model.Customer;
+import com.easybank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
+
+        Customer savedCustomer;
+        ResponseEntity response = null;
+
+        try {
+            savedCustomer = customerRepository.save(customer);
+            if (savedCustomer.getId() > 0) {
+                response = ResponseEntity
+                        .status(HttpStatus.CREATED)
+                        .body("Given user details are successfully registered");
+            }
+        } catch (Exception ex) {
+            response = ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An exception occurred to" + ex.getMessage());
+        }
+
+        return response;
+    }
+}
