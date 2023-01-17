e057874e472561ec77fc3fc72def518e27f5d857
Devang Chauhan <chauhan.devam@gmail.com>
2023-01-17 17:21:15 +0530

    EZB-14: refactored controllers code

--- src/main/java/com/easybank/controller/CardsController.java
+++ src/main/java/com/easybank/controller/CardsController.java
@@ -1,13 +1,28 @@
 package com.easybank.controller;
 
+import com.easybank.model.Cards;
+import com.easybank.repository.CardsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CardsController {
+
+    @Autowired
+    private CardsRepository cardsRepository;
+
     @GetMapping("/myCards")
-    public String cardsDetails(){
-        return "Here are the card details from the DB";
+    public List<Cards> getCardDetails(@RequestParam int id) {
+        List<Cards> cards = cardsRepository.findByCustomerId(id);
+        if (cards != null) {
+            return cards;
+        } else {
+            return null;
+        }
     }
 
 }
--- src/main/java/com/easybank/controller/LoginController.java
+++ src/main/java/com/easybank/controller/LoginController.java
@@ -1,35 +1,37 @@
 package com.easybank.controller;
 
-
 import com.easybank.model.Customer;
 import com.easybank.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
 
 @RestController
 public class LoginController {
 
     @Autowired
-    CustomerRepository customerRepository;
+    private CustomerRepository customerRepository;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
-
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
-
-        Customer savedCustomer;
+        Customer savedCustomer = null;
         ResponseEntity response = null;
-
         try {
             String hashPwd = passwordEncoder.encode(customer.getPwd());
             customer.setPwd(hashPwd);
+            customer.setCreateDt(String.valueOf(new Date(System.currentTimeMillis())));
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
@@ -39,9 +41,20 @@ public class LoginController {
         } catch (Exception ex) {
             response = ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("An exception occurred to" + ex.getMessage());
+                    .body("An exception occured due to " + ex.getMessage());
         }
-
         return response;
     }
+
+    @RequestMapping("/user")
+    public Customer getUserDetailsAfterLogin(Authentication authentication) {
+        List<Customer> customers = customerRepository.findByEmail(authentication.getName());
+        if (customers.size() > 0) {
+            return customers.get(0);
+        } else {
+            return null;
+        }
+
+    }
+
 }
